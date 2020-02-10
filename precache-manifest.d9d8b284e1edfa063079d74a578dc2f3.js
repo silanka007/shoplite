bb49876884e56f786c75d9097e5c863780dd337a
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-02-10 07:58:46 +0100

    Updates

---
+++ precache-manifest.d9d8b284e1edfa063079d74a578dc2f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fc3bbd116cf7b0af26d209861ee3f7",
+    "url": "/shoplite/index.html"
+  },
+  {
+    "revision": "3b638a5abe9311b264f8",
+    "url": "/shoplite/static/css/main.aaf29e61.chunk.css"
+  },
+  {
+    "revision": "0c3d862a28c0cafe6297",
+    "url": "/shoplite/static/js/2.480b0e33.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/shoplite/static/js/2.480b0e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b638a5abe9311b264f8",
+    "url": "/shoplite/static/js/main.e7b3ac5f.chunk.js"
+  },
+  {
+    "revision": "96584633b3df162a8b86",
+    "url": "/shoplite/static/js/runtime-main.a933448f.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/shoplite/static/media/shoplite.ed28accb.svg"
+  }
+]);
